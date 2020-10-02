e3dbc4b4420e3dadfe83c61ab6f18393bbc4f2a9
Rupesh Kumar Tiwari <roopkt@gmail.com>
2020-10-02 17:17:58 -0400

    added routes

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -12,8 +12,8 @@
 </head>
 
 <body>
-    <h1>About</h1>
-    <p>This is the about page</p>
+    <h1>Services</h1>
+    <p>This is the servcies page</p>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PagesController@index');
 
-Route::get('/about', function() {
-    return view('pages/about');
-});
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
